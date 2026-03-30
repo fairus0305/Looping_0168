71d3c017fbc46f32e7a73c79846f58ec751727ea
fairus0305 <ruszezzy@gmail.com>
2026-03-30 17:16:13 +0700

    menambah prosedur Output

--- switchCase.cpp
+++ switchCase.cpp
@@ -40,3 +40,12 @@ void pilihanJurusan()
         statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+// prosedur output
+void output()
+{
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
