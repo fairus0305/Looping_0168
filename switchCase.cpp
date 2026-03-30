11d4622e37571c3a1b8422358842f7c4d510696e
fairus0305 <ruszezzy@gmail.com>
2026-03-30 17:17:07 +0700

    menambahkan fungsi utama (main) untuk menjalankan alur program

--- switchCase.cpp
+++ switchCase.cpp
@@ -49,3 +49,12 @@ void output()
     cout << "Status Kuota    : " << statusKuota << endl;
     cout << "----------------------------------" << endl;
 }
+
+int main()
+{
+    input();
+    pilihanJurusan();
+    output();
+
+    return 0;
+}
